80452c603f3f44c2a833faf0cd748c44649cfc6d
LeventSoykan <levent.soykan@turkcell.com.tr>
2023-02-05 21:34:47 +0300

    Add numpydoc to current functions

--- turkish_chars/turkish_chars.py
+++ turkish_chars/turkish_chars.py
@@ -1,14 +1,38 @@
 
 
 tc_dict = {'İ': 'I', 'ı': 'i', 'Ö': 'O', 'ö': 'o', 'Ü': 'U', 'ü': 'u',
-                        'ğ': 'g', 'Ğ': 'G', 'Ş': 'S', 'ş': 's', 'Ç': 'C', 'ç': 'c'}
+           'ğ': 'g', 'Ğ': 'G', 'Ş': 'S', 'ş': 's', 'Ç': 'C', 'ç': 'c'}
 
 
 def convert_to_eng(txt_to_process):
+    """Convert the special Turkish characters in a text to English characters
+
+    Parameters
+    ----------
+    txt_to_process : str
+        Input text containing Turkish characters
+
+    Returns
+    -------
+    converted_text: str
+        Text with English characters
+    """
     return "".join([c if c not in tc_dict.keys() else tc_dict[c] for c in txt_to_process])
 
 
 def highlight_tc(txt_to_process):
+    """Print the text to console with Turkish characters highlighted as bold
+
+    Parameters
+    ----------
+    txt_to_process : str
+        Input text containing Turkish characters
+        
+
+    Returns
+    -------
+    None
+    """
     string_exp = "".join([c if c not in tc_dict.keys() else '\033[1m'+c+'\033[0m' for c in txt_to_process])
     print(string_exp)
 
