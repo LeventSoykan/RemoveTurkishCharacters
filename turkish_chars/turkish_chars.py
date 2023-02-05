7c0a3ee57fa51623e48fd99ad7e91be0247c16a3
LeventSoykan <levent.soykan@turkcell.com.tr>
2023-02-05 21:27:17 +0300

    Add function to highligh turkish characters

--- turkish_chars/turkish_chars.py
+++ turkish_chars/turkish_chars.py
@@ -1,19 +1,20 @@
 
-class TurkishChars:
-    def __init__(self, target_text):
-        self.text = target_text
-        self.tc_dict = {'İ': 'I', 'ı': 'i', 'Ö': 'O', 'ö': 'o', 'Ü': 'U', 'ü': 'u',
+
+tc_dict = {'İ': 'I', 'ı': 'i', 'Ö': 'O', 'ö': 'o', 'Ü': 'U', 'ü': 'u',
                         'ğ': 'g', 'Ğ': 'G', 'Ş': 'S', 'ş': 's', 'Ç': 'C', 'ç': 'c'}
-        self.eng = self.convert_to_eng(self.text)
 
-    def convert_to_eng(self, txt_to_process):
-        return "".join([c if c not in self.tc_dict.keys() else self.tc_dict[c] for c in txt_to_process])
 
+def convert_to_eng(txt_to_process):
+    return "".join([c if c not in tc_dict.keys() else tc_dict[c] for c in txt_to_process])
+
+
+def highlight_tc(txt_to_process):
+    string_exp = "".join([c if c not in tc_dict.keys() else '\033[1m'+c+'\033[0m' for c in txt_to_process])
+    print(string_exp)
 
 
 if __name__ == '__main__':
-    text = 'Sesi büzüşesiceler.'
-    tc = TurkishChars(text)
-    # print([text])
-    print(tc.eng)
-    
+    text = 'Sesi büzüşesiceler. Çılgın dağcan'
+    print(convert_to_eng(text))
+    highlight_tc(text)
+
